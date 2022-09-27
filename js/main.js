650be83582a8695fea330615afbcda1d1f79a606
AlterEnok <108426805+AlterEnok@users.noreply.github.com>
2022-09-27 18:15:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="">',
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1,
+                    
+                }
+            }
+        ]
+       
+    });
+});
